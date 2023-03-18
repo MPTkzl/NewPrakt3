b0dfb6058e2f1d1812f028791c02d1720fdb4c5e
MPTkzl <105377542+yye1337@users.noreply.github.com>
2023-03-18 17:38:42 +0300

    123

---
+++ NewPrakt3/FirstWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using NewPrakt3.qqqDataSetTableAdapters;
+
+namespace NewPrakt3
+{
+    /// <summary>
+    /// Логика взаимодействия для FirstWindow.xaml
+    /// </summary>
+    public partial class FirstWindow : Window
+    {
+        zakaziTableAdapter zakazi = new zakaziTableAdapter();
+        usersTableAdapter users = new usersTableAdapter();
+        int id = 0;
+        public FirstWindow()
+        {
+            InitializeComponent();
+            Zakazi.ItemsSource = zakazi.GetData();
+            userCb.ItemsSource = users.GetData();
+            userCb.DisplayMemberPath = "name";
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            zakazi.InsertQuery(Convert.ToInt32(NameTbx.Text), id, Convert.ToDecimal(NameTbx2.Text));
+            Zakazi.ItemsSource = zakazi.GetData();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            object id = (Zakazi.SelectedItem as DataRowView).Row[0];
+            zakazi.DeleteQuery(Convert.ToInt32(id));
+            Zakazi.ItemsSource = zakazi.GetData();
+        }
+
+        private void bt1_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void userCb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            object cell = (userCb.SelectedItem as DataRowView).Row[0];
+            id = Convert.ToInt32(cell);
+        }
+
+        
+    }
+}
---
+++ NewPrakt3/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NewPrakt3.qqqDataSetTableAdapters;
+
+namespace NewPrakt3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        usersTableAdapter users = new usersTableAdapter();
+        public MainWindow()
+        {
+            InitializeComponent();
+            Users.ItemsSource = users.GetData();
+        }
+
+        private void bt_Click(object sender, RoutedEventArgs e)
+        {
+            users.InsertQuery(Convert.ToInt32(NameTbx.Text), Convert.ToString(NameTbx1.Text), Convert.ToString(NameTbx2.Text));
+
+            Users.ItemsSource = users.GetData();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            object id = (Users.SelectedItem as DataRowView).Row[0];
+            users.DeleteQuery(Convert.ToInt32(id));
+            Users.ItemsSource = users.GetData();
+        }
+
+        private void bt1_Click(object sender, RoutedEventArgs e)
+        {
+            FirstWindow window1 = new FirstWindow();
+            window1.ShowDialog();
+        }
+    }
+}
